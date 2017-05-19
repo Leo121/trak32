96f6f7fd6561c682b6bf24dea31ec7199e3c6409
Phantron <keith@phantron.com>
2017-05-18 21:31:31 -0700

    I2C source update

--- Trak32.X/nbproject/Makefile-MX695.mk
+++ Trak32.X/nbproject/Makefile-MX695.mk
@@ -51,17 +51,17 @@ OBJECTDIR=build/${CND_CONF}/${IMAGE_TYPE}
 DISTDIR=dist/${CND_CONF}/${IMAGE_TYPE}
 
 # Source Files Quoted if spaced
-SOURCEFILES_QUOTED_IF_SPACED=Trak32.c trakhardware.c trakserial.c trakcommands.c trakLEDs.c trakbarometer.c trakeep.c trakaccel.c trakADC.c ../../Trak32.X/p32_i2c2.c
+SOURCEFILES_QUOTED_IF_SPACED=Trak32.c trakhardware.c trakserial.c trakcommands.c trakLEDs.c trakbarometer.c trakeep.c trakaccel.c trakADC.c p32_i2c2.c
 
 # Object Files Quoted if spaced
-OBJECTFILES_QUOTED_IF_SPACED=${OBJECTDIR}/Trak32.o ${OBJECTDIR}/trakhardware.o ${OBJECTDIR}/trakserial.o ${OBJECTDIR}/trakcommands.o ${OBJECTDIR}/trakLEDs.o ${OBJECTDIR}/trakbarometer.o ${OBJECTDIR}/trakeep.o ${OBJECTDIR}/trakaccel.o ${OBJECTDIR}/trakADC.o ${OBJECTDIR}/_ext/164137329/p32_i2c2.o
-POSSIBLE_DEPFILES=${OBJECTDIR}/Trak32.o.d ${OBJECTDIR}/trakhardware.o.d ${OBJECTDIR}/trakserial.o.d ${OBJECTDIR}/trakcommands.o.d ${OBJECTDIR}/trakLEDs.o.d ${OBJECTDIR}/trakbarometer.o.d ${OBJECTDIR}/trakeep.o.d ${OBJECTDIR}/trakaccel.o.d ${OBJECTDIR}/trakADC.o.d ${OBJECTDIR}/_ext/164137329/p32_i2c2.o.d
+OBJECTFILES_QUOTED_IF_SPACED=${OBJECTDIR}/Trak32.o ${OBJECTDIR}/trakhardware.o ${OBJECTDIR}/trakserial.o ${OBJECTDIR}/trakcommands.o ${OBJECTDIR}/trakLEDs.o ${OBJECTDIR}/trakbarometer.o ${OBJECTDIR}/trakeep.o ${OBJECTDIR}/trakaccel.o ${OBJECTDIR}/trakADC.o ${OBJECTDIR}/p32_i2c2.o
+POSSIBLE_DEPFILES=${OBJECTDIR}/Trak32.o.d ${OBJECTDIR}/trakhardware.o.d ${OBJECTDIR}/trakserial.o.d ${OBJECTDIR}/trakcommands.o.d ${OBJECTDIR}/trakLEDs.o.d ${OBJECTDIR}/trakbarometer.o.d ${OBJECTDIR}/trakeep.o.d ${OBJECTDIR}/trakaccel.o.d ${OBJECTDIR}/trakADC.o.d ${OBJECTDIR}/p32_i2c2.o.d
 
 # Object Files
-OBJECTFILES=${OBJECTDIR}/Trak32.o ${OBJECTDIR}/trakhardware.o ${OBJECTDIR}/trakserial.o ${OBJECTDIR}/trakcommands.o ${OBJECTDIR}/trakLEDs.o ${OBJECTDIR}/trakbarometer.o ${OBJECTDIR}/trakeep.o ${OBJECTDIR}/trakaccel.o ${OBJECTDIR}/trakADC.o ${OBJECTDIR}/_ext/164137329/p32_i2c2.o
+OBJECTFILES=${OBJECTDIR}/Trak32.o ${OBJECTDIR}/trakhardware.o ${OBJECTDIR}/trakserial.o ${OBJECTDIR}/trakcommands.o ${OBJECTDIR}/trakLEDs.o ${OBJECTDIR}/trakbarometer.o ${OBJECTDIR}/trakeep.o ${OBJECTDIR}/trakaccel.o ${OBJECTDIR}/trakADC.o ${OBJECTDIR}/p32_i2c2.o
 
 # Source Files
-SOURCEFILES=Trak32.c trakhardware.c trakserial.c trakcommands.c trakLEDs.c trakbarometer.c trakeep.c trakaccel.c trakADC.c ../../Trak32.X/p32_i2c2.c
+SOURCEFILES=Trak32.c trakhardware.c trakserial.c trakcommands.c trakLEDs.c trakbarometer.c trakeep.c trakaccel.c trakADC.c p32_i2c2.c
 
 
 CFLAGS=
@@ -83,7 +83,7 @@ ifneq ($(INFORMATION_MESSAGE), )
 endif
 	${MAKE}  -f nbproject/Makefile-MX695.mk dist/${CND_CONF}/${IMAGE_TYPE}/Trak32.X.${IMAGE_TYPE}.${OUTPUT_SUFFIX}
 
-MP_PROCESSOR_OPTION=32MX675F512L
+MP_PROCESSOR_OPTION=32MX695F512L
 MP_LINKER_FILE_OPTION=
 # ------------------------------------------------------------------------------------
 # Rules for buildStep: assemble
@@ -154,11 +154,11 @@ ${OBJECTDIR}/trakADC.o: trakADC.c  nbproject/Makefile-${CND_CONF}.mk
 	@${RM} ${OBJECTDIR}/trakADC.o 
 	@${FIXDEPS} "${OBJECTDIR}/trakADC.o.d" $(SILENT) -rsi ${MP_CC_DIR}../  -c ${MP_CC}  $(MP_EXTRA_CC_PRE) -g -D__DEBUG -D__MPLAB_DEBUGGER_ICD3=1 -fframe-base-loclist  -x c -c -mprocessor=$(MP_PROCESSOR_OPTION)  -MMD -MF "${OBJECTDIR}/trakADC.o.d" -o ${OBJECTDIR}/trakADC.o trakADC.c      $(COMPARISON_BUILD)  -DSYS_CLOCK=24000000
 	
-${OBJECTDIR}/_ext/164137329/p32_i2c2.o: ../../Trak32.X/p32_i2c2.c  nbproject/Makefile-${CND_CONF}.mk
-	@${MKDIR} "${OBJECTDIR}/_ext/164137329" 
-	@${RM} ${OBJECTDIR}/_ext/164137329/p32_i2c2.o.d 
-	@${RM} ${OBJECTDIR}/_ext/164137329/p32_i2c2.o 
-	@${FIXDEPS} "${OBJECTDIR}/_ext/164137329/p32_i2c2.o.d" $(SILENT) -rsi ${MP_CC_DIR}../  -c ${MP_CC}  $(MP_EXTRA_CC_PRE) -g -D__DEBUG -D__MPLAB_DEBUGGER_ICD3=1 -fframe-base-loclist  -x c -c -mprocessor=$(MP_PROCESSOR_OPTION)  -MMD -MF "${OBJECTDIR}/_ext/164137329/p32_i2c2.o.d" -o ${OBJECTDIR}/_ext/164137329/p32_i2c2.o ../../Trak32.X/p32_i2c2.c      $(COMPARISON_BUILD)  -DSYS_CLOCK=24000000
+${OBJECTDIR}/p32_i2c2.o: p32_i2c2.c  nbproject/Makefile-${CND_CONF}.mk
+	@${MKDIR} "${OBJECTDIR}" 
+	@${RM} ${OBJECTDIR}/p32_i2c2.o.d 
+	@${RM} ${OBJECTDIR}/p32_i2c2.o 
+	@${FIXDEPS} "${OBJECTDIR}/p32_i2c2.o.d" $(SILENT) -rsi ${MP_CC_DIR}../  -c ${MP_CC}  $(MP_EXTRA_CC_PRE) -g -D__DEBUG -D__MPLAB_DEBUGGER_ICD3=1 -fframe-base-loclist  -x c -c -mprocessor=$(MP_PROCESSOR_OPTION)  -MMD -MF "${OBJECTDIR}/p32_i2c2.o.d" -o ${OBJECTDIR}/p32_i2c2.o p32_i2c2.c      $(COMPARISON_BUILD)  -DSYS_CLOCK=24000000
 	
 else
 ${OBJECTDIR}/Trak32.o: Trak32.c  nbproject/Makefile-${CND_CONF}.mk
@@ -215,11 +215,11 @@ ${OBJECTDIR}/trakADC.o: trakADC.c  nbproject/Makefile-${CND_CONF}.mk
 	@${RM} ${OBJECTDIR}/trakADC.o 
 	@${FIXDEPS} "${OBJECTDIR}/trakADC.o.d" $(SILENT) -rsi ${MP_CC_DIR}../  -c ${MP_CC}  $(MP_EXTRA_CC_PRE)  -g -x c -c -mprocessor=$(MP_PROCESSOR_OPTION)  -MMD -MF "${OBJECTDIR}/trakADC.o.d" -o ${OBJECTDIR}/trakADC.o trakADC.c      $(COMPARISON_BUILD)  -DSYS_CLOCK=24000000
 	
-${OBJECTDIR}/_ext/164137329/p32_i2c2.o: ../../Trak32.X/p32_i2c2.c  nbproject/Makefile-${CND_CONF}.mk
-	@${MKDIR} "${OBJECTDIR}/_ext/164137329" 
-	@${RM} ${OBJECTDIR}/_ext/164137329/p32_i2c2.o.d 
-	@${RM} ${OBJECTDIR}/_ext/164137329/p32_i2c2.o 
-	@${FIXDEPS} "${OBJECTDIR}/_ext/164137329/p32_i2c2.o.d" $(SILENT) -rsi ${MP_CC_DIR}../  -c ${MP_CC}  $(MP_EXTRA_CC_PRE)  -g -x c -c -mprocessor=$(MP_PROCESSOR_OPTION)  -MMD -MF "${OBJECTDIR}/_ext/164137329/p32_i2c2.o.d" -o ${OBJECTDIR}/_ext/164137329/p32_i2c2.o ../../Trak32.X/p32_i2c2.c      $(COMPARISON_BUILD)  -DSYS_CLOCK=24000000
+${OBJECTDIR}/p32_i2c2.o: p32_i2c2.c  nbproject/Makefile-${CND_CONF}.mk
+	@${MKDIR} "${OBJECTDIR}" 
+	@${RM} ${OBJECTDIR}/p32_i2c2.o.d 
+	@${RM} ${OBJECTDIR}/p32_i2c2.o 
+	@${FIXDEPS} "${OBJECTDIR}/p32_i2c2.o.d" $(SILENT) -rsi ${MP_CC_DIR}../  -c ${MP_CC}  $(MP_EXTRA_CC_PRE)  -g -x c -c -mprocessor=$(MP_PROCESSOR_OPTION)  -MMD -MF "${OBJECTDIR}/p32_i2c2.o.d" -o ${OBJECTDIR}/p32_i2c2.o p32_i2c2.c      $(COMPARISON_BUILD)  -DSYS_CLOCK=24000000
 	
 endif
 
